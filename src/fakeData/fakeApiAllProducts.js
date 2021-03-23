b324f52e1b4b0d0915447a982065ceda6f437ff9
Himateja Merlapaka <iamhimateja@gmail.com>
2021-03-24 03:59:33 +0530

    Added fakeapi images temporarily as fakestoreapi.com is broken

--- src/fakeData/fakeApiAllProducts.js
+++ src/fakeData/fakeApiAllProducts.js
@@ -1,3 +1,24 @@
+import id_1_image from './fakeApiImages/81fPKd-2AYL._AC_SL1500_.webp';
+import id_2_image from './fakeApiImages/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.webp';
+import id_3_image from './fakeApiImages/71li-ujtlUL._AC_UX679_.webp';
+import id_4_image from './fakeApiImages/71YXzeOuslL._AC_UY879_.webp';
+import id_5_image from './fakeApiImages/71pWzhdJNwL._AC_UL640_QL65_ML3_.webp';
+import id_6_image from './fakeApiImages/61sbMiUnoGL._AC_UL640_QL65_ML3_.webp';
+import id_7_image from './fakeApiImages/71YAIFU48IL._AC_UL640_QL65_ML3_.webp';
+import id_8_image from './fakeApiImages/51UDEzMJVpL._AC_UL640_QL65_ML3_.webp';
+import id_9_image from './fakeApiImages/61IBBVJvSDL._AC_SY879_.webp';
+import id_10_image from './fakeApiImages/61U7T1koQqL._AC_SX679_.webp';
+import id_11_image from './fakeApiImages/71kWymZ+c+L._AC_SX679_.webp';
+import id_12_image from './fakeApiImages/61mtL65D4cL._AC_SX679_.webp';
+import id_13_image from './fakeApiImages/81QpkIctqPL._AC_SX679_.webp';
+import id_14_image from './fakeApiImages/81Zt42ioCgL._AC_SX679_.webp';
+import id_15_image from './fakeApiImages/51Y5NI-I5jL._AC_UX679_.webp';
+import id_16_image from './fakeApiImages/81XH0e8fefL._AC_UY879_.webp';
+import id_17_image from './fakeApiImages/71HblAHs5xL._AC_UY879_-2.webp';
+import id_18_image from './fakeApiImages/71z3kpMAYsL._AC_UY879_.webp';
+import id_19_image from './fakeApiImages/51eg55uWmdL._AC_UX679_.webp';
+import id_20_image from './fakeApiImages/61pHAEJ4NML._AC_UX679_.webp';
+
 const allProducts = [
   {
     id: 1,
@@ -6,7 +27,7 @@ const allProducts = [
     description:
       'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
     category: 'men',
-    image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    image: id_1_image,
     createdAt: Date.UTC(2020, 1, 12, 22, 33, 22, 11)
   },
   {
@@ -16,7 +37,7 @@ const allProducts = [
     description:
       'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
     category: 'men',
-    image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+    image: id_2_image,
     createdAt: Date.UTC(2020, 2, 12, 22, 33, 22, 11)
   },
   {
@@ -26,7 +47,7 @@ const allProducts = [
     description:
       'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
     category: 'men',
-    image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+    image: id_3_image,
     createdAt: Date.UTC(2020, 1, 13, 22, 33, 22, 11)
   },
   {
@@ -36,7 +57,7 @@ const allProducts = [
     description:
       'The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.',
     category: 'men',
-    image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+    image: id_4_image,
     createdAt: Date.UTC(2020, 1, 13, 22, 33, 22, 11)
   },
   {
@@ -46,7 +67,7 @@ const allProducts = [
     description:
       "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
     category: 'jewellery',
-    image: 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg',
+    image: id_5_image,
     createdAt: Date.UTC(2020, 1, 14, 22, 33, 22, 11)
   },
   {
@@ -56,7 +77,7 @@ const allProducts = [
     description:
       'Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.',
     category: 'jewellery',
-    image: 'https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg',
+    image: id_6_image,
     createdAt: Date.UTC(2020, 1, 15, 22, 33, 22, 11)
   },
   {
@@ -66,7 +87,7 @@ const allProducts = [
     description:
       "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
     category: 'jewellery',
-    image: 'https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg',
+    image: id_7_image,
     createdAt: Date.UTC(2020, 1, 12, 12, 33, 22, 11)
   },
   {
@@ -76,7 +97,7 @@ const allProducts = [
     description:
       'Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel',
     category: 'jewellery',
-    image: 'https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg',
+    image: id_8_image,
     createdAt: Date.UTC(2020, 1, 15, 14, 33, 22, 11)
   },
   {
@@ -86,7 +107,7 @@ const allProducts = [
     description:
       'USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system',
     category: 'electronics',
-    image: 'https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg',
+    image: id_9_image,
     createdAt: Date.UTC(2020, 1, 15, 15, 33, 22, 11)
   },
   {
@@ -96,7 +117,7 @@ const allProducts = [
     description:
       'Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)',
     category: 'electronics',
-    image: 'https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg',
+    image: id_10_image,
     createdAt: Date.UTC(2020, 1, 15, 15, 36, 22, 11)
   },
   {
@@ -106,7 +127,7 @@ const allProducts = [
     description:
       '3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.',
     category: 'electronics',
-    image: 'https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg',
+    image: id_11_image,
     createdAt: Date.UTC(2020, 1, 15, 16, 36, 22, 11)
   },
   {
@@ -116,7 +137,7 @@ const allProducts = [
     description:
       "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
     category: 'electronics',
-    image: 'https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg',
+    image: id_12_image,
     createdAt: Date.UTC(2020, 1, 15, 16, 45, 22, 11)
   },
   {
@@ -126,7 +147,7 @@ const allProducts = [
     description:
       '21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz',
     category: 'electronics',
-    image: 'https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg',
+    image: id_13_image,
     createdAt: Date.UTC(2020, 1, 17, 16, 23, 22, 11)
   },
   {
@@ -137,7 +158,7 @@ const allProducts = [
     description:
       '49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag',
     category: 'electronics',
-    image: 'https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg',
+    image: id_14_image,
     createdAt: Date.UTC(2020, 1, 17, 17, 23, 22, 14)
   },
   {
@@ -147,7 +168,7 @@ const allProducts = [
     description:
       'Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates',
     category: 'women',
-    image: 'https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg',
+    image: id_15_image,
     createdAt: Date.UTC(2020, 11, 17, 17, 23, 22, 14)
   },
   {
@@ -157,7 +178,7 @@ const allProducts = [
     description:
       '100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON',
     category: 'women',
-    image: 'https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg',
+    image: id_16_image,
     createdAt: Date.UTC(2020, 3, 14, 17, 23, 43, 14)
   },
   {
@@ -167,7 +188,7 @@ const allProducts = [
     description:
       "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
     category: 'women',
-    image: 'https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg',
+    image: id_17_image,
     createdAt: Date.UTC(2019, 3, 14, 17, 23, 43, 14)
   },
   {
@@ -177,7 +198,7 @@ const allProducts = [
     description:
       '95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem',
     category: 'women',
-    image: 'https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg',
+    image: id_18_image,
     createdAt: Date.UTC(2021, 1, 14, 17, 23, 43, 14)
   },
   {
@@ -187,7 +208,7 @@ const allProducts = [
     description:
       '100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort',
     category: 'women',
-    image: 'https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg',
+    image: id_19_image,
     createdAt: Date.UTC(2021, 2, 28, 17, 23, 12, 14)
   },
   {
@@ -197,7 +218,7 @@ const allProducts = [
     description:
       '95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.',
     category: 'women',
-    image: 'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg',
+    image: id_20_image,
     createdAt: Date.UTC(2021, 3, 1, 9, 23, 12, 14)
   },
 ];
